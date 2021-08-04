a150123d30628c3e4c8ac68a8504c2ab8efa28aa
Allencaidota <56335140+Allencaidota@users.noreply.github.com>
2021-08-04 12:15:59 -0700

    study

--- EFDataAccessLibrary/Models/Address.cs
+++ EFDataAccessLibrary/Models/Address.cs
@@ -1,13 +1,28 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EFDataAccessLibrary
 {
     public class Address
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
         public string StreetAddress { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string City { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string State { get; set; }
+
+        [Required]
+        [MaxLength(10)] //1235-0000
+        [Column(TypeName = "varchar(10)")]
         public string ZipCode { get; set; }
 
     }
--- EFDataAccessLibrary/Models/Person.cs
+++ EFDataAccessLibrary/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace EFDataAccessLibrary.Models
@@ -7,8 +8,14 @@ namespace EFDataAccessLibrary.Models
     public class Person
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
         public string FirstName { get; set; }
+        [Required]
+        [MaxLength(200)]
         public string LastName { get; set; }
+        [Required]
         public int Age { get; set; }
         public List<Address> Addresses { get; set; } = new List<Address>();
         public List<Email> EmaildAddresses { get; set; } = new List<Email>();
