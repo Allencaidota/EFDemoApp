1efbc6b7da240b5d2457aeb22b2074608f9347f8
Allencaidota <56335140+Allencaidota@users.noreply.github.com>
2021-08-08 21:56:36 -0700

    try sampledatate with loadsampledata

--- EFDemoWeb/Pages/Index.cshtml.cs
+++ EFDemoWeb/Pages/Index.cshtml.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace EFDemoWeb.Pages
@@ -11,15 +15,33 @@ namespace EFDemoWeb.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly PeopleContext _db;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, PeopleContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public void OnGet()
         {
+            LoadSampleData();
+            var people = _db.people
+                .Include(a => a.Addresses)
+                .Include(e => e.EmaildAddresses)
+                .ToList();
 
+        }
+
+        private void LoadSampleData()
+        {
+            if(_db.people.Count() == 0)
+            {
+                string file = System.IO.File.ReadAllText("generated.json");
+                var people = JsonSerializer.Deserialize<List<Person>>(file);
+                _db.AddRange(people);
+                _db.SaveChanges();
+            }
         }
     }
 }
